375f6b5e97ff64854b98ff8dedbc686bd2ff9929
hyseart <164656769+hyseart@users.noreply.github.com>
2024-08-12 16:21:24 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,6 +12,13 @@
       );
     });
 
+    $(".jq--scroll-lottery").click(function () {
+      $("html, body").animate(
+        { scrollTop: $(".jq--lottery").offset().top },
+        1300
+      );
+    });
+
     $(".jq--scroll-photogallery").click(function () {
       $("html, body").animate(
         { scrollTop: $(".jq--photogallery").offset().top },
@@ -25,5 +32,39 @@
         1300
       );
     });
+
+    /***** mobile navigation *****/
+    $(document).ready(function () {
+      $(".menu > li > a").click(function (event) {
+        event.preventDefault();
+
+        $(".submenu").slideToggle();
+      });
+    });
   });
 })(jQuery);
+
+function generateNumbers() {
+  const numberCount = 6;
+  const maxNumber = 27;
+  const additionalNumberRange = 4;
+  let numbers = [];
+
+  // hlavné čísla
+  while (numbers.length < numberCount) {
+    let randomNumber = Math.floor(Math.random() * maxNumber) + 1;
+    if (!numbers.includes(randomNumber)) {
+      numbers.push(randomNumber);
+    }
+  }
+  numbers.sort(function (a, b) {
+    return a - b;
+  });
+
+  // doplnkové číslo
+  let additionalNumber = Math.floor(Math.random() * additionalNumberRange) + 1;
+
+  document.querySelector(".numbers").innerText = `${numbers.join(
+    ", "
+  )} | Doplnkové číslo: ${additionalNumber}`;
+}
